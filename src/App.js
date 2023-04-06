fed2edfb1c6ac8409e7c046e669a372f287e77cc
Demozy4321 <satyajittoppo012@gmail.com>
2023-04-06 21:19:25 +0530

    added counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleMinus = () => {
+    setCount(count - 1);
+  };
+
+  const handlePlus = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>COUNTER APP</h1>
       </header>
+      <div className="content">
+        <button className="btn" onClick={handleMinus}>
+          -
+        </button>
+        <h1 className="counter">{count}</h1>
+        <button className="btn" onClick={handlePlus}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
